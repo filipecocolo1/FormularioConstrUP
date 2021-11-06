888efab3c1065b8713502842790947a7e2325275
filipecocolo1 <53791311+filipecocolo1@users.noreply.github.com>
2021-11-05 21:52:23 -0300

    Formulario Constr UP Finalizado

---
+++ Formulario Constr UP/js/scripts.js
@@ -0,0 +1,104 @@
+function validarform(){
+
+const name = document.getElementById('name');
+const descricao = document.getElementById('descricao');
+const marca = document.getElementById('marca');
+const quantidade = document.getElementById('quantidade');
+const mercadoriaId = document.getElementById('quantidade');
+
+  if(name.value ==''){
+    name.classList.add("errorInput");
+
+  }else{
+    name.classList.removed("errorInput");
+
+  }S
+    if(descricao.value ==''){
+        descricao.classList.add("errorInput");
+     }else{
+    descricao.classList.removed("errorInput");
+  }
+    if(marca.value ==''){
+       marca.classList.add("errorInput");
+      }else{
+    marca.classList.removed("errorInput");
+  }
+  
+  if(quantidade.value ==''){
+       quantidade.classList.add("errorInput");
+      }else{
+    marca.classList.removed("errorInput");
+  }
+
+
+if(mercadoriaId.value ==''){
+       mercadoriaId.classList.add("errorInput");
+      }  else{
+    mercadoriaId.classList.removed("errorInput");
+  }    
+
+
+  if(mercadoriaId.value.length<= 5){
+       mercadoriaId.classList.add("errorInput");
+  
+  }else{
+    mercadoriaId.classList.removed("errorInput");
+
+   
+ 
+  }    
+ 
+
+}
+function validarform(){
+
+const name = document.getElementById('name');
+const descricao = document.getElementById('descricao');
+const marca = document.getElementById('marca');
+const quantidade = document.getElementById('quantidade');
+const mercadoriaId = document.getElementById('quantidade');
+
+  if(name.value ==''){
+    name.classList.add("errorInput");
+
+  }else{
+    name.classList.removed("errorInput");
+
+  }S
+    if(descricao.value ==''){
+        descricao.classList.add("errorInput");
+     }else{
+    descricao.classList.removed("errorInput");
+  }
+    if(marca.value ==''){
+       marca.classList.add("errorInput");
+      }else{
+    marca.classList.removed("errorInput");
+  }
+  
+  if(quantidade.value ==''){
+       quantidade.classList.add("errorInput");
+      }else{
+    marca.classList.removed("errorInput");
+  }
+
+
+if(mercadoriaId.value ==''){
+       mercadoriaId.classList.add("errorInput");
+      }  else{
+    mercadoriaId.classList.removed("errorInput");
+  }    
+
+
+  if(mercadoriaId.value.length<= 5){
+       mercadoriaId.classList.add("errorInput");
+  
+  }else{
+    mercadoriaId.classList.removed("errorInput");
+
+   
+ 
+  }    
+ 
+
+}
